f234b5a63d21929f6024673ea26adb9a258bfb79
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2017-04-21 13:15:05 +0530

    shmem working

--- ipc/shmem/src/files/bin/shmem.sh
+++ ipc/shmem/src/files/bin/shmem.sh
@@ -1,1 +1,5 @@
-echo --- this is from shmem ----
+echo --- shmem recv ----
+echo --- to run send override run command with --command=send ---
+echo --- you can run two applications with overriden command for shmem b/w two ---
+echo --- or simpley run send command at system console ---
+/app/bin/recv
