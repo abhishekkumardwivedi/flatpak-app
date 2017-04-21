860d1f0a66a44ef224ddaafbce3b145605179d49
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2017-04-21 15:32:58 +0530

    socket working

--- socket/src/files/bin/socket.sh
+++ socket/src/files/bin/socket.sh
@@ -1,1 +1,6 @@
-echo "this is from socket app"
+echo "----- socket app --------"
+echo "---- server running ---------"
+echo --- to check for two way communication override run command with below -----
+echo --command=client
+
+/app/bin/server
